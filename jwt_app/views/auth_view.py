094b4fd49c1c6cb632c24bc6a4f472b71d871b99
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-20 12:38:20 +0545

    Register View Done

--- jwt_app/views/auth_view.py
+++ jwt_app/views/auth_view.py
@@ -1,5 +1,6 @@
-from rest_framework import response, status
+from rest_framework import status
 from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from ..serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
@@ -12,4 +13,18 @@ def register_view(request):
         email = request.data.get('email')
         password = request.data.get('password')
         
-        return print(username, email, password)
+        if not username or not email or not password:
+            return Response({'error': 'All fields are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(email=email).exists():
+            return Response({'error': 'Email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        if user is not None:
+            return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
+        return Response({'error': 'Failed to create user.'}, status=status.HTTP_400_BAD_REQUEST)
+
