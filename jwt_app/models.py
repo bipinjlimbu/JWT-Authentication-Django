90a171911516f686254e9abf18084997d188709c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-20 13:18:46 +0545

    Product Model and Serializer Created

--- jwt_app/models.py
+++ jwt_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- jwt_app/serializers.py
+++ jwt_app/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Product
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+        
+class ProductSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'user', 'created_at']
